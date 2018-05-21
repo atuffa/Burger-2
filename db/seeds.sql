a5391f632a704c664a0274c388fa8a8dc1f0a14c
osman144 <osman144@umn.edu>
2018-05-21 14:09:13 -0500

    changes to db

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name,devoured) VALUES('chicken burger', FALSE); 
+INSERT INTO burgers(burger_name,devoured)VALUES('cheese burger', FALSE);
+INSERT INTO burgers(burger_name,devoured) VALUES ('vegan', FALSE)
